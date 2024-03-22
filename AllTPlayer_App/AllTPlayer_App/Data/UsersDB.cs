c7ee059abfa31a842b30ec6524ee1a99da897619
AllTP_3356 <kostinsasa0023@gmail.com>
2024-03-23 00:36:36 +0300

    Добавление функционала для импорта музыки из аккаунта VK

---
+++ AllTPlayer_App/AllTPlayer_App/Data/UsersDB.cs
@@ -0,0 +1,56 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AllTPlayer_App.Models;
+
+namespace AllTPlayer_App.Data
+{
+    public class UsersDB
+    {
+        readonly SQLiteAsyncConnection db;
+
+        public UsersDB(string connection)
+        {
+            db = new SQLiteAsyncConnection(connection);
+
+            db.CreateTableAsync<User>().Wait();
+        }
+
+        public Task<List<User>> GetUsersAsync()
+        {
+            return db.Table<User>().ToListAsync();
+        }
+
+        public Task<int> DeleteUsersAsync()
+        {
+            return db.DeleteAllAsync<User>();
+        }
+
+        public Task<int> SaveUserAsync(User user)
+        {
+            if (user.Id != 0)
+            {
+                return db.UpdateAsync(user);
+            }
+            else
+            {
+                return db.InsertAsync(user);
+            }
+        }
+
+
+
+        // Future features ------------------------------------
+        public Task<User> GetUserAsync(int id)
+        {
+            return db.Table<User>().Where(i=> i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> DeleteUserAsync(User user) 
+        {
+            return db.DeleteAsync(user);
+        }
+
+
+    }
+}
